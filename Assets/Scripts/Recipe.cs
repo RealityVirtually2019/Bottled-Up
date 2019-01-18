607cbb578b7d4361eed90c664c6a20e28d288f9e
Daniel Dangond <dangond@mit.edu>
2019-01-18 15:03:03 -0500

    Set up recipe book data structure

---
+++ Assets/Scripts/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Recipe
+{
+    public Dictionary<IngredientType, int> recipeIngredients;
+    public PotionType createdPotionType;
+
+    public bool matchesRecipe(Dictionary<IngredientType, int> ingredients)
+    {
+        return ingredients.Equals(recipeIngredients);
+    }
+}
---
+++ Assets/Scripts/RecipeBook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipeBook : MonoBehaviour
+{
+    public List<Recipe> recipes;
+
+    private Recipe GetRecipe(Dictionary<IngredientType, int> testRecipe)
+    {
+        foreach (Recipe recipe in recipes)
+        {
+            if (recipe.matchesRecipe(testRecipe))
+            {
+                return recipe;
+            }
+        }
+        return null;
+    }
+
+    public PotionType GetBrewingResult(Dictionary<IngredientType, int> testRecipe)
+    {
+        if (testRecipe.Keys.Count == 0)
+        {
+            return PotionType.NONE;
+        }
+        Recipe recipe = GetRecipe(testRecipe);
+        if (recipe == null)
+        {
+            return PotionType.MISTAKE;
+        }
+        return recipe.createdPotionType;
+    }
+}
