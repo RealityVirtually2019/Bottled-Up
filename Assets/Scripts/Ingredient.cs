5f0f1e996c3679701384f6905408070d8c7ad5e1
Daniel Dangond <dangond@mit.edu>
2019-01-18 12:10:07 -0500

    Set up ingredient logic

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Ingredient : MonoBehaviour
 {
+    public GameObject preparedForm;
     public ToolType toolType;
+    public int hitsToPrepare = 3;
+    private int hitsSoFar = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +19,35 @@ public class Ingredient : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider otherCollider)
+    {
+        Tool collidedTool = otherCollider.gameObject.GetComponent<Tool>();
+        if (collidedTool == null)
+        {
+            return;
+        }
+
+        if (collidedTool.toolType == toolType)
+        {
+            HitByTool();
+        }
+    }
+
+    private void HitByTool()
+    {
+        hitsSoFar += 1;
+        if (hitsSoFar == hitsToPrepare)
+        {
+            ReplaceWithPreparedForm();
+        }
+    }
+
+    private void ReplaceWithPreparedForm()
+    {
+        Transform parentTransform = gameObject.GetComponentInParent<Transform>();
+        Instantiate(preparedForm, parentTransform.position, parentTransform.rotation);
+        Destroy(gameObject);
     }
 }
